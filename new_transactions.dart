0672033afb2fe6bb01d47be3293ebf2f433788e1
Kartikey Sinha <52502376+kartikeysinha@users.noreply.github.com>
2020-07-16 00:56:30 +0530

    Update new_transactions.dart
    Allowed users to pick dates on which they consumed food.

--- new_transactions.dart
+++ new_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -11,21 +12,35 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
-
   final calorieController = TextEditingController();
+  DateTime _selectedDate;
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+    print('...');
+  }
 
   void submitData() {
     final enteredTitle = titleController.text;
     final enteredCal = double.parse(calorieController.text);
 
-    if (enteredTitle.isEmpty || enteredCal <= 0){
+    if (enteredTitle.isEmpty || enteredCal < 0 || _selectedDate == null) {
       return;
     }
 
-    widget.addTx(
-      enteredTitle,
-      enteredCal,
-    );
+    widget.addTx(enteredTitle, enteredCal, _selectedDate);
 
     Navigator.of(context).pop();
   }
@@ -49,18 +64,40 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               onSubmitted: (_) => submitData(),
             ),
-            RaisedButton(
-              onPressed: null,
-              child: Text('Date'),
-              color: Theme.of(context).accentColor,
-              textColor: Theme.of(context).textTheme.button.color,
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen'
+                          : DateFormat.yMd().format(_selectedDate),
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Colors.black,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _presentDatePicker,
+                  ),
+                ],
+              ),
             ),
-            FlatButton(
+            RaisedButton(
               onPressed: () {
-                widget.addTx(titleController.text, double.parse(calorieController.text),);
+                widget.addTx(
+                  titleController.text,
+                  double.parse(calorieController.text),
+                );
               },
               child: Text('Add Transaction'),
               textColor: Theme.of(context).textTheme.button.color,
+              color: Theme.of(context).primaryColor,
             )
           ],
         ),
